a4892db8a8dfc8c9e299c3384e59f0e74ec14b37
Rafael Valentim Fonseca <rvalentim@correios.com.br>
2025-08-15 09:38:46 -0300

    Ajustando e testando as chamadas à api newsapi.

--- shared/src/commonMain/kotlin/com/rafael/dailypulse/articles/ArticlesUseCase.kt
+++ shared/src/commonMain/kotlin/com/rafael/dailypulse/articles/ArticlesUseCase.kt
@@ -1,5 +1,14 @@
 package com.rafael.dailypulse.articles
 
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.daysUntil
+import kotlinx.datetime.toLocalDateTime
+import kotlinx.datetime.todayIn
+import kotlin.math.abs
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
+import kotlin.time.Instant
+
 //Classe responsável por mapear oa dados do response nas classes de negócio do sistema.
 class ArticlesUseCase(private val service : ArticlesService) {
 
@@ -13,8 +22,26 @@ class ArticlesUseCase(private val service : ArticlesService) {
         Article(
             raw.title,
             raw.desc ?: "Click to find out more",
-            raw.date,
+            getDaysAgoString(raw.date),
             raw.imageUrl ?:"https://images.unsplash.com/photo-1609743522653-52354461eb27?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         )
+    }
+
+    @OptIn(ExperimentalTime::class)
+    private fun getDaysAgoString(date : String): String {
+        val today = Clock.System.todayIn(TimeZone.currentSystemDefault())
+        val days = today.daysUntil(
+            Instant.parse(date).toLocalDateTime(TimeZone.currentSystemDefault()).date
+        )
+
+        val result = when {
+            abs(days) > 1 -> "${days} days ago"
+            abs(days) == 1 -> "Yesterday"
+            else -> "Today"
+        }
+
+        return  result
+
+
     }
 }
