d1d0298f3022b9eeff165ccaa8c0acbd8e47d88d
Rafael Valentim Fonseca <rvalentim@correios.com.br>
2025-08-15 10:20:14 -0300

    Ajustando e testando as chamadas à api newsapi.

---
+++ shared/src/commonMain/kotlin/com/rafael/dailypulse/articles/di/ArticlesModule.kt
@@ -0,0 +1,16 @@
+package com.rafael.dailypulse.articles.di
+
+import com.rafael.dailypulse.articles.ArticlesService
+import com.rafael.dailypulse.articles.ArticlesUseCase
+import com.rafael.dailypulse.articles.ArticlesViewModel
+import org.koin.dsl.module
+
+//Creating the injection dependency.
+val articlesModule = module {
+
+    single <ArticlesService>{ArticlesService(get())}
+    single<ArticlesUseCase>{ ArticlesUseCase(get()) }
+    single<ArticlesViewModel>{ ArticlesViewModel() }
+
+
+}
