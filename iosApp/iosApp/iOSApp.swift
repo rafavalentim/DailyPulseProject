9881644821896e55aed2f755339ac13be4e2d70c
Rafael Valentim Fonseca <rvalentim@correios.com.br>
2025-08-18 10:50:03 -0300

    Finalizando a implementação de injeção de dependência no IOS.

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,15 @@
 import SwiftUI
+import shared
 
 @main
 struct iOSApp: App {
+    
+    init(){
+        
+        KoinInitializeKt.doInitKoin()
+        
+    }
+    
 	var body: some Scene {
 		WindowGroup {
 			ContentView()
---
+++ shared/src/iosMain/kotlin/com/rafael/dailypulse/di/KoinInitialize.kt
@@ -0,0 +1,19 @@
+package com.rafael.dailypulse.di
+
+import com.rafael.dailypulse.articles.ArticlesViewModel
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import org.koin.core.context.startKoin
+
+fun initKoin(){
+    val modules = sharedKoinModules
+
+    startKoin {
+        modules(modules)
+    }
+}
+
+class ArticlesInjector: KoinComponent {
+    val articlesViewModel: ArticlesViewModel by inject()
+}
+
