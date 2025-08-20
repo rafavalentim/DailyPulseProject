5d034cf4e5374bf50ae1b97c799cc9a0a7322f9b
Rafael Valentim Fonseca <rvalentim@correios.com.br>
2025-08-20 11:48:38 -0300

    Implementando a classe DataSource para acesso ao banco de dados.

---
+++ shared/src/commonMain/kotlin/com/rafael/dailypulse/articles/ArticlesDataSource.kt
@@ -0,0 +1,44 @@
+package com.rafael.dailypulse.articles
+
+import com.rafael.dailypulse.db.DailyPulseDatabase
+
+
+class ArticlesDataSource(private val database: DailyPulseDatabase) {
+
+    fun getAllArticles(): List<ArticlesRaw> = database.dailyPulseDatabaseQueries.selectAllArticles(:: mapToArticleRaw).executeAsList()
+
+    fun insertArticles(articles : List<ArticlesRaw>) {
+        database.dailyPulseDatabaseQueries.transaction{
+            articles.forEach { articlesRaw ->
+                insertArticle(articlesRaw)
+            }
+        }
+    }
+
+    fun clearArticles() = database.dailyPulseDatabaseQueries.removeAllArticles()
+
+
+    private fun insertArticle(articlesRaw: ArticlesRaw){
+        database.dailyPulseDatabaseQueries.insertArticle(
+            articlesRaw.title,
+            articlesRaw.desc,
+            articlesRaw.date,
+            articlesRaw.imageUrl
+        )
+    }
+
+
+    private fun mapToArticleRaw(
+        title: String,
+        desc: String?,
+        date: String,
+        url: String?
+    ) : ArticlesRaw =
+        ArticlesRaw(
+            title,
+            desc,
+            date,
+            url
+        )
+
+}
