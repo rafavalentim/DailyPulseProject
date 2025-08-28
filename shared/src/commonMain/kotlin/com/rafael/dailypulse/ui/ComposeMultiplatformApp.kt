72e0978e9ca1a3382e6d397757f462b3925aa0da
Rafael Valentim Fonseca <rvalentim@correios.com.br>
2025-08-28 16:18:20 -0300

    Finalizando a parte de implementação para IOS e Android com a implementação de transições entre as telas.

--- shared/src/commonMain/kotlin/com/rafael/dailypulse/ui/ComposeMultiplatformApp.kt
+++ shared/src/commonMain/kotlin/com/rafael/dailypulse/ui/ComposeMultiplatformApp.kt
@@ -1,11 +1,8 @@
 package com.rafael.dailypulse.ui
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.navigator.Navigator
